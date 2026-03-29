de8a4fc31ac0775382881d455261891193a3a9e7
Xarzy <erfbdes@gmail.com>
2026-03-30 01:54:00 +0200

    Usar Speed Insights de React en vez de Next.js

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from 'react-router';
 import { ThemeProvider } from 'next-themes';
 import { router } from './routes';
 import { AppProvider } from './context/AppContext';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
