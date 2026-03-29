40e7cc506cf1d3fe4237f9b3f39ecf67a38cdd96
Xarzy <erfbdes@gmail.com>
2026-03-30 01:46:36 +0200

    Añadir Speed Analytics

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,13 +2,17 @@ import { RouterProvider } from 'react-router';
 import { ThemeProvider } from 'next-themes';
 import { router } from './routes';
 import { AppProvider } from './context/AppContext';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <AppProvider>
         <RouterProvider router={router} />
+        <SpeedInsights />
       </AppProvider>
+      <Analytics />
     </ThemeProvider>
   );
 }
