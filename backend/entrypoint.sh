43cda1dbfaa1b1ef83915536fbf40fc242d868da
Xarzy <erfbdes@gmail.com>
2026-04-07 20:54:05 +0200

    fix entrypoint y añadir .dockerignore

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,17 +7,12 @@ until nc -z "$DB_HOST" "${DB_PORT:-5432}"; do
 done
 echo "DB ready."
 
-echo "Running migrations..."
-python manage.py migrate --no-input
-
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
-echo "Seeding categories..."
-python manage.py seed_categories || true
-
 echo "Starting server..."
-exec gunicorn backend.wsgi:application \
+ls -l ..
+gunicorn backend.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 2 \
   --timeout 60 \
